3d67eced940018132e6d4a8ecd9c8284ee353e95
DhifliMohamed <Mohamed_Dhifli@hotmail.com>
2018-10-06 23:57:14 +0100

    00

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ const express = require('express');
 const app = express();
 var Request = require("request");
 
+app.set('view engine', 'ejs')
 
-Request.get("https://google.com", (error, response, body) => {
-   if(error) {
-        return console.log("err");
-    }
-    console.dir(body);
-});
+
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.render(__dirname + '/index.ejs')
     // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/result', (req, res) => {
+    Request.get("https://facebook.com", (error, response, body) => {
+        if(error) {
+            resu='err';
+            res.render(__dirname + '/result.ejs',{resu:resu});
+            return console.log("err");
+         }
+         resu=body;
+        res.render(__dirname + '/result.ejs',{resu:resu});
+        return console.log("okk");
+     });
 })
 
 app.listen(80, function() {
